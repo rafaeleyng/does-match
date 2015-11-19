7dccf7f8ca79e7f110c72ea4c5cafd44235b1844
Rafael Eyng <rafaeleyng@gmail.com>
2015-11-19 18:57:12 -0200

    Add CommonJS support

--- does-match.js
+++ does-match.js
@@ -1,113 +1,130 @@
-/*
-  helpers
-*/
-var MULTIPLIERS = {
-  MATCH_WHOLE: 4,
-  MATCH_WORD: 2
-};
+(function(factory) {
+  // establish the root object
+  var root = (typeof self == 'object' && self.self == self && self) ||
+            (typeof global == 'object' && global.global == global && global);
 
-var replaceDiacritics = function(c) {
-  'àáãâ'.indexOf(c)>-1 && (c = 'a');
-   'èéê'.indexOf(c)>-1 && (c = 'e');
-   'ìíî'.indexOf(c)>-1 && (c = 'i');
-   'òóô'.indexOf(c)>-1 && (c = 'o');
-   'ùúû'.indexOf(c)>-1 && (c = 'u');
-     'ç'.indexOf(c)>-1 && (c = 'c');
-     'ñ'.indexOf(c)>-1 && (c = 'n');
-  return c;
-};
-
-var validateText = function(text) {
-  if (typeof text !== 'string') {
-    throw new Error('`text` should be string');
+  // node.js/commonjs
+  if (typeof exports !== 'undefined') {
+    module.exports = factory(root);
+  // browser global
+  } else {
+    root.doesMatch = factory(root);
   }
-};
 
-var validateQuery = function(query) {
-  if (typeof query !== 'string') {
-    throw new Error('`query` should be string');
-  }
-};
+}(function(root) {
+  /*
+    helpers
+  */
+  var MULTIPLIERS = {
+    MATCH_WHOLE: 4,
+    MATCH_WORD: 2
+  };
 
-/*
-  match
-*/
-var matchChars = function(charWord, charQuery) {
-  return replaceDiacritics(charWord) === replaceDiacritics(charQuery);
-};
+  var replaceDiacritics = function(c) {
+    'àáãâ'.indexOf(c)>-1 && (c = 'a');
+     'èéê'.indexOf(c)>-1 && (c = 'e');
+     'ìíî'.indexOf(c)>-1 && (c = 'i');
+     'òóô'.indexOf(c)>-1 && (c = 'o');
+     'ùúû'.indexOf(c)>-1 && (c = 'u');
+       'ç'.indexOf(c)>-1 && (c = 'c');
+       'ñ'.indexOf(c)>-1 && (c = 'n');
+    return c;
+  };
 
-var matchWhole = function(text, query) {
-  if (text.indexOf(query)>-1) {
-    return query.length * MULTIPLIERS.MATCH_WHOLE;
-  }
-  return 0;
-};
+  var validateText = function(text) {
+    if (typeof text !== 'string') {
+      throw new Error('`text` should be string');
+    }
+  };
 
-var matchWords = function(text, query) {
-  var queryWords = query.split(' ').filter(function(word) {
-    return word.length > 3; // only account for words bigger than 3
-  });
-  return queryWords.reduce(function(acc, word) {
-    var didMatch = text.indexOf(word)>-1;
-    return acc + (didMatch ? word.length * MULTIPLIERS.MATCH_WORD : 0);
-  }, 0);
-};
+  var validateQuery = function(query) {
+    if (typeof query !== 'string') {
+      throw new Error('`query` should be string');
+    }
+  };
 
-var matchLookahead = function(text, query) {
-  query = query.replace(/ /g, '');
+  /*
+    match
+  */
+  var matchChars = function(charWord, charQuery) {
+    return replaceDiacritics(charWord) === replaceDiacritics(charQuery);
+  };
 
-  var relevance = 0;
-  for (var i in query) {
-    var charQuery = query[i];
-    var didFindChar = false;
-    var isAdjacent = true;
-    for (var j in text) {
-      var charText = text[j];
-      if (matchChars(charQuery, charText)) {
-        didFindChar = true;
-        break;
+  var matchWhole = function(text, query) {
+    if (text.indexOf(query)>-1) {
+      return query.length * MULTIPLIERS.MATCH_WHOLE;
+    }
+    return 0;
+  };
+
+  var matchWords = function(text, query) {
+    var queryWords = query.split(' ').filter(function(word) {
+      return word.length > 3; // only account for words bigger than 3
+    });
+    return queryWords.reduce(function(acc, word) {
+      var didMatch = text.indexOf(word)>-1;
+      return acc + (didMatch ? word.length * MULTIPLIERS.MATCH_WORD : 0);
+    }, 0);
+  };
+
+  var matchLookahead = function(text, query) {
+    query = query.replace(/ /g, '');
+
+    var relevance = 0;
+    for (var i in query) {
+      var charQuery = query[i];
+      var didFindChar = false;
+      var isAdjacent = true;
+      for (var j in text) {
+        var charText = text[j];
+        if (matchChars(charQuery, charText)) {
+          didFindChar = true;
+          break;
+        }
+        isAdjacent = false;
       }
-      isAdjacent = false;
+      if (isAdjacent) {
+        relevance++;
+      }
+      if (!didFindChar) {
+        return 0;
+      }
+      text = text.substring(parseInt(j) + 1); // on next iteration, will look in the text hereinafter
     }
-    if (isAdjacent) {
-      relevance++;
+    return relevance;
+  }
+
+  /*
+    api
+  */
+  var doesMatch = function(text, query) {
+    // validate
+    validateText(text);
+    validateQuery(query);
+
+    // arrange
+    text = text.toLowerCase();
+    query = query.toLowerCase();
+
+    // matches
+    // whole match
+    var wholeMatch = matchWhole(text, query)
+    if (wholeMatch) {
+      return wholeMatch;
     }
-    if (!didFindChar) {
-      return 0;
+    // words match
+    var wordsMatch = matchWords(text, query)
+    if (wordsMatch) {
+      return wordsMatch;
     }
-    text = text.substring(parseInt(j) + 1); // on next iteration, will look in the text hereinafter
-  }
-  return relevance;
-}
+    // lookahead match
+    var lookaheadMatch = matchLookahead(text, query);
+    if (lookaheadMatch) {
+      return lookaheadMatch;
+    }
 
-/*
-  api
-*/
-var doesMatch = function(text, query) {
-  // validate
-  validateText(text);
-  validateQuery(query);
+    return 0;
+  };
 
-  // arrange
-  text = text.toLowerCase();
-  query = query.toLowerCase();
-
-  // matches
-  // whole match
-  var wholeMatch = matchWhole(text, query)
-  if (wholeMatch) {
-    return wholeMatch;
-  }
-  // words match
-  var wordsMatch = matchWords(text, query)
-  if (wordsMatch) {
-    return wordsMatch;
-  }
-  // lookahead match
-  var lookaheadMatch = matchLookahead(text, query);
-  if (lookaheadMatch) {
-    return lookaheadMatch;
-  }
-
-  return 0;
-};
+  return doesMatch;
+}));
